469833631a7947d4601fab58bcf68791ccf21e8a
dharamconjoniix <dharam.kirti@conjoinix.com>
2023-12-13 10:24:16 +0530

    update index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -183,8 +183,7 @@ export default function Home() {
                 Welcome To Next Tailwind
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+                Lorem ipsum dolor sit amet
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
