ff83bf69b84cc25c65c4611d2f6931cfb3d0e5cb
dharamconjoniix <dharam.kirti@conjoinix.com>
2023-12-13 10:35:16 +0530

    update index file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,10 @@ import type { AppProps } from 'next/app'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <SpeedInsights />
+  </> 
+  );
 }
