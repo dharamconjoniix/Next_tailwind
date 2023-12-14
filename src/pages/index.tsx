8ee455829e791b8414f8913ddc88b350076e101d
dharamconjoniix <dharam.kirti@conjoinix.com>
2023-12-14 09:39:48 +0530

    Update index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,6 +50,7 @@ const people = [
 
 ]
 
+
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
@@ -293,7 +294,7 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+      {/* <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
             <div className="max-w-xl lg:max-w-lg">
@@ -354,7 +355,7 @@ export default function Home() {
             }}
           />
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
